7fc52cc46bf409372e3a8f5091584908ce15795f
ardszsantos <lokgamer01@gmail.com>
2025-01-12 14:00:40 -0300

    feat: removed the /landing route and made the deafult route to render the landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { ErrorPage } from "./pages/404";
 import { ComingSoonPage } from "./pages/ComingSoon";
 import { TestingPage } from "./pages/TestingPage";
 import { LandingPage } from "./pages/Landing";
+import { AskAQuestion } from "./pages/AskAQuestion";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<TestingPage />} />        {/* Página principal */}
+        <Route path="/" element={<LandingPage />} />        {/* Página principal */}
         <Route path="/coming-soon" element={<ComingSoonPage />} /> {/* Outra página */}
         <Route path="*" element={<ErrorPage />} />              {/* Página 404 */}
-        <Route path="test" element={<TestingPage />} />              {/* Página 404 */}
-        <Route path="landing" element={<LandingPage />} />              {/* Página 404 */}
+        <Route path="test" element={<TestingPage />} />              {/* Pagina de testes */}
+        <Route path="askaquestion" element={<AskAQuestion />} />              {/* Faça uma pergunta */}
       </Routes>
     </>
   );
