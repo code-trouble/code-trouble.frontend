e1e29946cc334a53ad5bbeb7dda8c1d4b5c868db
ardszsantos <lokgamer01@gmail.com>
2025-01-20 14:29:16 -0300

    feat: re-adding the filter modal component as it was removed from here.

--- src/pages/Questions/index.tsx
+++ src/pages/Questions/index.tsx
@@ -4,8 +4,8 @@ import { Footer } from "../../components/Footer";
 import CustomButton from "../../components/CustomButton";
 import { QuestionsPreview } from "../../components/QuestionPreview";
 import { Tag } from "../../components/Tag";
-
 import filterSettings from "../../assets/images/svg/filterSettings.svg";
+import { QuestionsFilterModal } from "../../components/QuestionsFilterModal";
 
 
 const tags = ["Design", "Programação", "Arte", "Ciência de Dados", "Tecnologia"]
@@ -88,94 +88,7 @@ export const Questions: React.FC = () => {
             </div>
             <Footer customStyle={{bottom: 0}}/>
             {isModalOpen && (
-                <div className="modal-overlay" onClick={toggleModal}>
-                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                        <div className="modal-header">
-                            <div className="modal-checkbox-list">
-                                <p>Filtrar por</p>
-                                <div className="inputs">
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Sem respostas</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="checkbox"/>
-                                        <label>Sem resposta aceitada</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Com Bounty</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="modal-checkbox-list">
-                                <p>Classificado por</p>
-                                <div className="inputs secondCol">
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Mais recentes</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Atividade Recente</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Mais respostas</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Bounty prestes á expirar</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="modal-checkbox-list">
-                                <p>Classificado por</p>
-                                <div className="inputs">
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>Minhas Tags seguidas</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="checkbox" />
-                                        <label>As seguintes Tags</label>
-                                    </div>
-                                    <div className="checkbox-div">
-                                        <input type="text" placeholder="ex; javascript, python, sql....." />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <div className="modal-buttons">
-                                <CustomButton 
-                                    backgroundColor="#3348A4"
-                                    color="#FAFCFE"
-                                    fontWeight="400"
-                                    fontSize="16px"
-                                    text="Aplicar Filtro"
-                                    padding="8px 12.5px"
-                                />
-                                <CustomButton 
-                                    backgroundColor="white"
-                                    color="#3348A4"
-                                    fontWeight="400"
-                                    fontSize="16px"
-                                    text="Salvar filtro customizado"
-                                    padding="8px 12.5px"
-                                    border="1px solid #3348A4"
-                                />
-                            </div>
-                                <CustomButton 
-                                    backgroundColor="transparent"
-                                    color="#BA2D2F"
-                                    fontWeight="400"
-                                    fontSize="16px"
-                                    text="Cancelar"
-                                />
-                        </div>
-                    </div>    
-                </div>
+                <QuestionsFilterModal onClick={toggleModal}/>
             )}
         </div>
     );
