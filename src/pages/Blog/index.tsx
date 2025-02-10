24898665e54749255856b72c10d2a469bb347262
ardszsantos <lokgamer01@gmail.com>
2025-02-10 13:34:09 -0300

    feat: continued the development of the blog page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import CustomButton from "../../components/CustomButton";
+import { Tag } from "../../components/Tag";
 
 
 export const Blog: React.FC = () => {
     
+    const tags = ["Design", "Programação", "Arte", "Ciência de Dados", "Tecnologia"];
+
+
     return (
         <div className="blog-container">
             <Header theme="blue" loggedIn={false}/>
@@ -22,7 +26,7 @@ export const Blog: React.FC = () => {
                             padding="8px 41px"
                         />
                     </div>
-                    <div className="category-selection">
+                    <div className="category-selection-blue">
                         <a href="#">Para Você</a>
                         <a href="#">Seguindo</a>
                         <a href="#">Design</a>
@@ -31,18 +35,32 @@ export const Blog: React.FC = () => {
                         <a href="#">UI</a>
                     </div>
                     <div className="blog-list">
-                        
+
                     </div>  
                 </div>
 
-                <div className="topics-area">
-                    <div className="recommended-topics">
-                        <h2>Tópicos Recomendados</h2>
-                        
+                <div className="right-side">
+                    <div className="community-choices">
+                        <h2>Escolhas da Comunidade</h2>
+                    </div>
+
+                    <div className="topics-area">
+                        <div className="recommended-topics">
+                            <h2>Tópicos Recomendados</h2>
+                            <div className="tag-group">
+                                <Tag tags={tags} />
+                            </div>
+                            <span>Ver mais tópicos</span>
+                        </div>
+                    </div>
+
+                    <div className="who-to-follow">
+                        <h2>Quem Seguir</h2>
                     </div>
                 </div>
+
             </div>
-            <Footer customStyle={{bottom: 0, position: "fixed"}}/>
+            <Footer/>
         </div>
     );
     
