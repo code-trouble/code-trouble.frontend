520044d489afa09f3e67cbbe3f3398cbf2407fef
ardszsantos <ardszsantos>
2025-02-16 19:01:06 +0000

    feat: re-wrote the blog preview code, still not where i want it to be tho...

--- src/components/BlogPostPreview/index.tsx
+++ src/components/BlogPostPreview/index.tsx
@@ -13,33 +13,38 @@ interface IBlogPostPreview {
 
 export const BlogPostPreview: React.FC<IBlogPostPreview> = ({ blogPostTitle, blogPostDescription, image }) => {
     return (
-        <div className="blog-wrapper" style={image ? { display: "flex", alignItems: "center" } : {}}>
-            <div className="blog-post">
-                <Avatar sizes="medium" name="Joana Lima"/>
-                <div className="blog-post-content">
-                    <div className="blog-post-text">
-                        <h1>{blogPostTitle}</h1>
-                        <p>{blogPostDescription}</p>
+        <div className="outer-post-wrpaper">
+            <div className="post-wrapper">
+                <div className="post-content">
+                    <Avatar name="Joana Lima" sizes="medium"/>
+                    <h1 className="blog-post-title">{blogPostTitle}</h1>
+                    <p className="blog-post-description">{blogPostDescription}</p>
+                    <div className="blog-post-footer">
+                        <div className="main-footer">
+                            <span>10 Nov, 2024</span>
+                            <p><img src={heart}/> 120k</p>
+                            <p><img src={comments}/> 302</p>
+                        </div>
+                        <div className="sub-footer">
+                            <img src={favorite} alt="" />
+                        </div>
                     </div>
                 </div>
-                <div className="blog-post-footer">
-                    <div className="blog-post-footer-left">
-                        <p className="date-p">10 Nov, 2024</p>
-                        <p>
-                            <img src={heart} alt="heart" />
-                            120K
-                        </p>
-                        <p>
-                            <img src={comments} alt="comments" />
-                            302
-                        </p>
-                    </div>
-                    <div>
-                        <img src={favorite} alt="favorite icon"/>
-                    </div>
+                <div className="post-image">
+                    {image && <img src={dogImage} alt=""/>}
+                </div>
+
+            </div>
+            <div className="blog-post-mobile-footer">
+                <div className="main-footer">
+                    <span>10 Nov, 2024</span>
+                    <p><img src={heart}/> 120k</p>
+                    <p><img src={comments}/> 302</p>
+                </div>
+                <div className="sub-footer">
+                    <img src={favorite} alt="" />
                 </div>
             </div>
-            {image && <img src={dogImage} alt=""/>}
-        </div>
+        </div>               
     );
 };
