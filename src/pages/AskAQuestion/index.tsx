b2136569f4a5435a187958d82a6c4af3dac794fb
ardszsantos <lokgamer01@gmail.com>
2025-02-26 12:39:19 -0300

    feat: created the message-boxes for the mobile screen

--- src/pages/AskAQuestion/index.tsx
+++ src/pages/AskAQuestion/index.tsx
@@ -11,11 +11,33 @@ export const AskAQuestion: React.FC = () => {
             <div className="askAquestion-wrapper">
                 <div className="left-side">
                     <h1>Fazer uma pergunta</h1>
-            
+                    <div className="message-box">
+                        <h1>Como escrever uma boa pergunta</h1>
+                        <p>Você precisa perguntar algo? Esse formulário irá te ajudar durante o processo.</p>
+                        <ul>
+                            <span className="tittle-span">Passos a se seguir:</span>
+                            <li>Resuma sua questão em um título curto, que retrate bem o que precisa.</li>
+                            <li>Descreva seu problema com mais detalhes.</li>
+                            <li>Descreva o que você já tentou e o que espera que aconteça.</li>
+                            <li>Adicione <span>tags</span> para que sua pergunta seja entregue para os membros certos da comunidade.</li>
+                            <li>Revise sua pergunta antes de postar.</li>
+                            <li>E pronto, agora só postar!</li>
+                        </ul>
+                    </div>
                     <div className="title-area">
                         <h6>Título</h6>
                         <p>Seja específico e imagine que está fazendo uma pergunta para outra pessoa.</p>
                         <input className="input-area" type="text" placeholder="Ex: Como fazer um Array no javascript" />
+                    </div>
+                    <div className="message-box second-box">
+                        <h1>Escrevendo um bom título</h1>
+                        <div className="imageMessage-box">
+                            <img src={correa} alt="" />
+                            <div className="text-div">
+                                <p>O título da sua pergunta deve resumir bem o seu problema.</p>
+                                <p>Lembre-se sempre de usar palavras chave, para a sua pergunta ter mais chance de aperecer em pesquisas dos usuários.</p>
+                            </div>
+                        </div>
                     </div>
                     <div className="question-details">
                         <h6>Detalhes da pergunta</h6>
@@ -27,10 +49,30 @@ export const AskAQuestion: React.FC = () => {
                         <p>Descreva o que voce tentou, o que era para acontecer e o que realmente aconteceu (como o erro, por exemplo). Mínimo de 20 caractéres.</p>
                         <AltPostWriter/>
                     </div>
+                    <div className="message-box second-box">
+                        <h1>Revise antes de postar</h1>
+                        <div className="imageMessage-box">
+                            <img src={correa} alt="" />
+                            <div className="text-div">
+                                <p>Agora que está pronto para postar sua pergunta, releia do começo ao fim. Ela faz sentido?</p>
+                                <p>Adicione quaisquer detalhes que possa ter esquecido e releia novamente. Agora é um bom momento para ter certeza de que o título ainda descreve bem o seu problema.</p>
+                            </div>
+                        </div>
+                    </div>
                     <div className="tag-area">
                         <h6>Tags</h6>
                         <p>Adicione até 5 tags para descrever sobre o que sua pergunta é.</p>
                         <input className="input-area" type="text" placeholder="Ex: javascript, array, front-end, etc" />
+                    </div>
+                    <div className="message-box second-box">
+                        <h1>Adicionando tags</h1>
+                        <div className="imageMessage-box">
+                            <img src={correa} alt="" />
+                            <div className="text-div">
+                                <p>As tags garantem que sua pergunta terá atenção das pessoas certas.</p>
+                                <p>Adicione tags de linguagens ou tecnologias usadas.</p>
+                            </div>
+                        </div>
                     </div>
                     <div className="revision-message">
                         <p>Antes de postar a sua pergunta, faça uma revisão final.</p>
@@ -43,7 +85,8 @@ export const AskAQuestion: React.FC = () => {
                                     color="white"
                                     fontSize="18px"
                                     fontWeight="500"
-                                    borderRadius="8px"/>
+                                    borderRadius="8px"
+                                    customId="posteButton"/>
                         <CustomButton text="Descartar pergunta" color="#BA2D2F"/>
                     </div>
 
