ccc9b478a54d3fa4cea188b7aabf2b76df085379
ardszsantos <lokgamer01@gmail.com>
2025-06-04 16:29:27 -0300

    feat: made progress on this page

--- src/pages/OpenQuestion/index.tsx
+++ src/pages/OpenQuestion/index.tsx
@@ -19,6 +19,7 @@ import comments from '../../assets/images/svg/greenComments.svg'
 import upvotes from '../../assets/images/svg/greenUpvote.svg'
 import addToFavorite from '../../assets/images/svg/addToFavorite.svg'
 import threeDotMenu from '../../assets/images/svg/3dotsMenu.svg'
+import { Avatar } from "../../components/Avatar";
 
 export const OpenQuestion: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -88,6 +89,45 @@ export const OpenQuestion: React.FC = () => {
             <img src={threeDotMenu} alt="menu with 3 dots" />
           </div>
         </div>
+        <div className="questionCreator">
+          <p>Criador(a) da pergunta</p>
+          <div className="followUser">
+            <Avatar sizes="large" name="Joana Lima" role="" src=""/> 
+            <div className="followButton">
+                <span className="dot"/>
+                <p>Seguir</p>
+            </div>
+          </div>
+        </div>
+        <div className="answersArea">
+          <div className="upperAnswers">
+            <p>1 Resposta</p>
+            <div className="filterAnswers">
+              <p>Ordenar por:</p>
+              <div className="filterAnswersDropdown">
+                <select className="order-select">
+                  <option value="newest">Mais recentes</option>
+                  <option value="oldest">Mais antigas</option>
+                  <option value="top">Mais curtidas</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="answerDisplayBlock">
+              <div className="answerUserArea">
+                <Avatar sizes="large" name="Olivia Ryes"/>
+              </div>
+              <div className="answerText">
+
+              </div>
+          </div>
+        </div>
+
+
+
+
+
         {/* <button
           onClick={() => {
             remove(question.id);
