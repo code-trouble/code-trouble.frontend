bd27c63a48fd233f07b66b52e2fda1dc2c6243a1
ardszsantos <lokgamer01@gmail.com>
2025-08-26 14:17:47 -0300

    feat: setting up the api service, with axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useAuthStore } from "../stores/authStore";
 
 const apiBaseUrl = import.meta.env.DEV
   ? "http://localhost:3000"
@@ -12,36 +11,3 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-let onTokenExpired: (() => void) | null = null;
-
-export const setTokenExpiredHandler = (handler: () => void) => {
-  onTokenExpired = handler;
-};
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("authToken");
-      useAuthStore.getState().logout();
-      if (onTokenExpired) {
-        onTokenExpired();
-      }
-    }
-    return Promise.reject(error);
-  },
-);
