6c39dd90ac6a14100107b36315c971dfabbf3240
ardszsantos <lokgamer01@gmail.com>
2025-09-02 11:54:31 -0300

    feat: changing the default dev url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBaseUrl = import.meta.env.DEV
-  ? "http://localhost:3000"
+  ? "http://localhost:8080"
   : import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
