9ce2d34da0b662d34bbfdd42073ea1613b026810
ardszsantos <lokgamer01@gmail.com>
2025-09-05 14:16:44 -0300

    feat: made several adittions to the onboarding types

--- src/types/onboardingSectionTypes.ts
+++ src/types/onboardingSectionTypes.ts
@@ -1,5 +1,31 @@
+interface ProfileDetails {
+  display_name: string;
+  username: string;
+  pronouns: string;
+  bio: string;
+}
+
+export interface SocialLinks {
+  linkedin: string;
+  github: string;
+  dribbble: string;
+  behance: string;
+}
+
 export interface onboardingSectionProps {
   sectionTitle: string;
   sectionDescription: string;
   sectionContent: React.ReactNode;
+}
+
+export interface OnboardingState {
+  interestTagIds: Set<number>;
+  profileDetails: ProfileDetails;
+  socialLinks: SocialLinks;
+
+  setInterests: (updateIds: Set<number>) => void;
+  updateProfileDetail: (field: keyof ProfileDetails, value: string) => void;
+  updateSocialLink: (field: keyof SocialLinks, value: string) => void;
+
+  resetOnboardingState: () => void;
 }
