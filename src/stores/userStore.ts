45968fdf5c6386342a725a001d51f50ac32035fb
ardszsantos <lokgamer01@gmail.com>
2025-09-10 15:01:17 -0300

    feat: changing the isLOading state to isInitializing, this prevents a global state refresh when dealing with user actions

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -4,43 +4,41 @@ import { User, UserState, UpdateProfileData } from "../types/userTypes";
 
 export const useUserStore = create<UserState>((set) => ({
   currentUser: null,
-  isLoading: true,
+  isInitializing: true,
+
+  setUser: (user: User) => set({ currentUser: user, isInitializing: false }),
 
   fetchCurrentUser: async () => {
-    set({ isLoading: true });
+    set({ isInitializing: true });
     try {
       const response = await api.get<User>("/users/me");
-      set({ currentUser: response.data, isLoading: false });
+      set({ currentUser: response.data, isInitializing: false });
     } catch (error) {
-      set({ currentUser: null, isLoading: false });
+      set({ currentUser: null, isInitializing: false });
+      throw error;
     }
   },
 
   updateProfile: async (data: UpdateProfileData) => {
-    set({ isLoading: true });
     try {
       const response = await api.patch<User>("/users/me", data);
 
-      set({ currentUser: response.data, isLoading: false });
+      set({ currentUser: response.data });
     } catch (error) {
       console.error("Falha ao atualizar o perfil:", error);
-      set({ isLoading: false });
       throw error;
     }
   },
 
   updateUserInterests: async (tagIds: number[]) => {
-    set({ isLoading: true });
     try {
       const response = await api.put<User>("/users/me/interests", { tagIds });
-      // Atualiza o usuário com a resposta que inclui os novos interesses
-      set({ currentUser: response.data, isLoading: false });
+      set({ currentUser: response.data });
     } catch (error) {
       console.error("Falha ao atualizar interesses:", error);
-      set({ isLoading: false });
       throw error;
     }
   },
 
-  clearUser: () => set({ currentUser: null, isLoading: false }),
+  clearUser: () => set({ currentUser: null, isInitializing: false }),
 }));
