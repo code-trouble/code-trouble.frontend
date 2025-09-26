b50bd361d37da1b1898d668cd7409ea987290cdf
ardszsantos <lokgamer01@gmail.com>
2025-09-26 16:28:51 -0300

    feat: built a generc post preview component

---
+++ src/components/GenericPostPreview/index.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Avatar } from "../Avatar";
+import comments from "../../assets/images/svg/icons/blueComment.svg";
+import heart from "../../assets/images/svg/icons/blueHeart.svg";
+import favorite from "../../assets/images/svg/icons/favoriteBlog.svg";
+import dogImage from "../../assets/images/png/dogImage.png";
+import { Post } from "../../types/postTypes";
+import { formatDate } from "../../utils/formatDate";
+
+interface PostPreviewProps {
+  post: Post;
+  postTitle: string;
+  postDescription: string;
+  image?: boolean;
+}
+
+export const PostPreview: React.FC<PostPreviewProps> = ({
+  post,
+  postTitle,
+  postDescription,
+  image,
+}) => {
+  const formattedDate = formatDate(post.created_at);
+  return (
+    <div className="post-preview-container">
+      <div className="post-preview">
+        <div className="post-preview-content">
+          <Avatar
+            name={post.author.display_name}
+            sizes="medium"
+            src={post.author.avatar_url}
+          />
+          <h1 className="post-preview-title">{postTitle}</h1>
+          <p className="post-preview-description">{postDescription}</p>
+          <div className="post-preview-footer">
+            <div className="footer-stats">
+              <span>{formattedDate}</span>
+              <p>
+                <img src={heart} alt="like icon" /> {post.likeCount || 0}
+              </p>
+              <p>
+                <img src={comments} alt="comments icon" /> 0
+              </p>
+            </div>
+            <div className="footer-actions">
+              <img src={favorite} alt="favorite icon" />
+            </div>
+          </div>
+        </div>
+        {image && (
+          <div className="post-preview-image">
+            <img src={dogImage} alt="Post placeholder image" />
+          </div>
+        )}
+      </div>
+      <div className="post-preview-mobile-footer">
+        <div className="footer-stats">
+          <span>{formattedDate}</span>
+          <p>
+            <img src={heart} alt="like icon" /> {post.likeCount || 0}
+          </p>
+          <p>
+            <img src={comments} alt="comments icon" /> 0
+          </p>
+        </div>
+        <div className="footer-actions">
+          <img src={favorite} alt="mobile favorite icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
